87bbbf4ec9953e1b03f3fa8c4ae7aa9e419cbcec
Hassan Abbas <habbas375@gmail.com>
2021-02-06 23:37:29 -0600

    Put Convert function into main

--- main.py
+++ main.py
@@ -2,23 +2,6 @@ import sys
 from skimage import io
 
 
-def convert():
-    filepath = sys.argv[1]
-    image = io.imread(filepath, as_gray=True)
-    height, width = image.shape
-    resize_height_factor = 1
-    resize_width_factor = 1
-    if height > 1000:
-        resize_height_factor += height // 1000
-    if width > 1000:
-        resize_width_factor += width // 1000
-    result = open("output.txt", "w")
-    for row in range(0, height, 3 * resize_height_factor):
-        for col in range(0, width, 1 * resize_width_factor):
-            result.write(get_character(image[row, col]))
-        result.write("\n")
-
-
 def get_character(pixel):
     if pixel == 1.0:
         return " "
